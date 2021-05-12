4c9e3e73d1a817bcb2f45be1a927ae81c71f6272
Alan Basurto <alanrbasurto@gmail.com>
2021-05-12 17:48:28 -0500

    sin env

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -3,11 +3,17 @@ import "firebase/auth";
 import "firebase/firestore";
 
 firebase.initializeApp({
-   
+   apiKey: process.env.REACT_APP_APPIKEY,
+   authDomain: process.env.REACT_APP_AUTHDOMAIN,
+   projectId: process.env.REACT_APP_PROJECTID,
+   storageBucket: process.env.REACT_APP_STORAGEBUCKET,
+   messagingSenderId: process.env.REACT_APP_MESSAGINSENDERID,
+   appId: process.env.REACT_APP_APPID,
+   measurementId: process.env.REACT_APP_MEASUREMENTID
 });
 
 const auth = firebase.auth();
 const firestore = firebase.firestore();
 const googleProvider = new firebase.auth.GoogleAuthProvider();
 
-export {auth, firebase, firestore, googleProvider}
+export { auth, firebase, firestore, googleProvider }
